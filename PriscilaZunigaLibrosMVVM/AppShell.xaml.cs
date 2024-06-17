34f78cc3e9f57977743a67b5b61d40d02795e2a3
cisco <cisco@UPEAULA-31713>
2024-06-17 10:40:54 -0500

    Agregar archivos de proyecto.

---
+++ PriscilaZunigaLibrosMVVM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PriscilaZunigaLibrosMVVM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PriscilaZunigaLibrosMVVM/Models/PZLibro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriscilaZunigaLibrosMVVM.Models
+{
+    public class PZLibro
+    {
+        public string? Titulo { get; set; }
+        public string? Autor { get; set; }
+    }
+}
